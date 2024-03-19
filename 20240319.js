c451e4bfaa299cc54f0cb447de8f49668ae8e301
SouthKorea-UchihaObito <ctallhack@naver.com>
2024-03-19 17:56:22 +0900

    [ts]
    - 타입 변수 강의 듣는중

---
+++ 20240319.js
@@ -0,0 +1,3 @@
+// type alias 만드는 법 타입 변수
+// type Animal = string | number | undefined;
+let 동물 = { name: 'shim', age: 25 };
---
+++ 20240319.ts
@@ -0,0 +1,8 @@
+// type alias 만드는 법 타입 변수
+// type Animal = string | number | undefined;
+
+// let 동물 : Animal = 123;
+type Animal = {name : string , age : number};
+let 동물 : Animal = {name : 'shim', age : 25};
+
+
